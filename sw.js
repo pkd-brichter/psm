79e3c001ab3c7d5a5989ca5d0c3a2db18da298d5
Abbas-Hoseiny <abbas.hoseiny@gmx.de>
2026-06-24 06:53:06 +0200

    deploy: Anbauplan-Board + Ernte-Zeitraum + Plan/Erledigt + Kontextmenü

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * - Auto-Startup ohne Dialog (wenn Datenbank vorhanden)
  */
 
-const CACHE_VERSION = 'v2.0.0-1782261327776';
+const CACHE_VERSION = 'v2.0.0-1782276744416';
 const STATIC_CACHE = `psm-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `psm-dynamic-${CACHE_VERSION}`;
 const DB_STATE_KEY = 'psm-db-state';
